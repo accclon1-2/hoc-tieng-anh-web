ff33793b93acea3f616be2b7c98e80cd1088ca3b
accclon1-2 <accclonthumot@gmail.com>
2026-04-20 20:42:21 +0700

    Update hethongweb.py

--- hethongweb.py
+++ hethongweb.py
@@ -16,10 +16,8 @@ st.markdown("""
 # --- QUẢN LÝ DỮ LIỆU ---
 @st.cache_data
 def load_data():
-    try:
         with open("data.json", "r", encoding="utf-8") as f:
             return json.load(f)
-    except: return {}
 
 data = load_data()
 
